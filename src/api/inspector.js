21fd8d88579b878a79ebff90e007f8c85cfbcf31
TomorrowStarrySky <2046374535@qq.com>
2024-06-14 14:38:18 +0800

    supervisor.js and inspector.js update

--- src/api/inspector.js
+++ src/api/inspector.js
@@ -1,1 +1,21 @@
 import axios from './axios';
+
+// 网格员登录
+export function login(data){
+    return axios.post('/inspector/login', data)
+}
+
+// 网格员获取详细事务信息
+export function getDetailedInfo(infoId){
+    return axios.get('/inspector/detailedInfo/?infoId=${infoId}')
+}
+
+// 网格员反馈实时数据信息
+export function feedbackInfo(data){
+    return axios.post('/inspector/feedbackInfo', data)
+}
+
+// 网格员获取对应事务列表
+export function getInfoList(inspectorId){
+    return axios.get('/inspector/infoList/?inspectorId=${inspectorId}')
+}
--- src/api/supervisor.js
+++ src/api/supervisor.js
@@ -1,1 +1,26 @@
 import axios from './axios';
+
+// 公众监督员登录
+export function supervisorLogin(data) {
+    return axios.post('/supervisor/login', data);
+}
+
+// 公众监督员注册
+export function supervisorRegister(data) {
+    return axios.post('/supervisor/register', data);
+}
+
+// 编辑公众监督员个人信息
+export function editPersonal(data) {
+    return axios.post('/supervisor/editPersonal', data);
+}
+
+// 查询公众监督员历史反馈信息列表
+export function getAllFeedbackList(data) {
+    return axios.get('/supervisor/getAllFeedbackList');
+}
+
+// 公众监督员反馈监督信息
+export function addInfo(data) {
+    return axios.post('/supervisor/addInfo', data);
+}
