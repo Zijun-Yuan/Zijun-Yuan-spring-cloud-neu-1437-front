127ab2b6831980139e6a49892f634fd5b54b5717
Pei <1025792037@qq.com>
2024-06-20 11:45:52 +0800

    inspector board update

--- src/pages/inspector/InspectorBoard.vue
+++ src/pages/inspector/InspectorBoard.vue
@@ -13,9 +13,8 @@
             <el-button type="primary" @click="showCompleted">已完成</el-button>
           </el-col>
         </el-row>
-
+        <!-- 这里为未完成任务的表格 -->
         <div v-if="showContent === 'uncompleted'">
-          <p>显示未完成的内容</p>
           <el-table :data="uncompletedInfoList">
             <el-table-column label="省份">
               <template #default="{ row }">
@@ -32,9 +31,8 @@
             <el-table-column prop="time" label="时间" />
           </el-table>
         </div>
-
+        <!-- 这里为已完成任务的表格 -->
         <div v-else-if="showContent === 'completed'">
-          <p>显示已完成的内容</p>
           <el-table :data="completedInfoList">
             <el-table-column label="省份">
               <template #default="{ row }">
@@ -70,8 +68,6 @@ export default {
     const inspectorStore = useInspectorStore();
     const locationStore = useLocationStore();
     const infoList = ref([]);
-    // const processedInfoList = ref([]);
-
     const uncompletedInfoList = ref([]);
     const completedInfoList = ref([]);
 
@@ -90,16 +86,19 @@ export default {
       }
     };
 
+    // 展示未完成的任务列表
     const showUncompleted = async () => {
       showContent.value = 'uncompleted';
       await fetchInfoList();
     };
 
+    // 展示已完成的任务列表
     const showCompleted = async () => {
       showContent.value = 'completed';
       await fetchInfoList();
     };
 
+    // 获取省份信息
     const getProvince = async (cityCode) => {
       try {
         return await locationStore.getProvinceByCityCode(cityCode);
@@ -109,6 +108,7 @@ export default {
       }
     }
 
+    // 获取城市信息
     const getCity = async (cityCode) => {
       try {
         return await locationStore.getCityAndProvinceByCityCode(cityCode);
@@ -118,27 +118,23 @@ export default {
       }
     }
 
+    // 处理任务列表（未完成和已完成 以及 省份城市信息的获取）
     const processInfoList = async () => {
       const items = infoList.value;
       const uncompletedItems = [];
       const completedItems = [];
-      // const processedItems = [];
 
       for (const item of items) {
         const province = await getProvince(item.cityCode);
         const city = await getCity(item.cityCode);
         if (province && city) {
-          // processedItems.push({
-          //   ...item,
-          //   province: province,
-          //   city: city
-          // });
           const processedItem = {
             ...item,
             province: province,
             city: city
           };
-
+          // 处理状态
+          // 0: 已删除 1: 公众监督员supervisor已提交 2: 已指派网格员inspector 3: 网格员inspector已填写
           if (item.status === 3) {
             completedItems.push(processedItem);
           } else if (item.status === 2) {
@@ -146,7 +142,6 @@ export default {
           }
         }
       }
-      // processedInfoList.value = processedItems;
       completedInfoList.value = completedItems;
       uncompletedInfoList.value = uncompletedItems;
     };
@@ -163,7 +158,6 @@ export default {
       infoList,
       uncompletedInfoList,
       completedInfoList,
-      // processedInfoList,
       getProvince,
       getCity,
     };
